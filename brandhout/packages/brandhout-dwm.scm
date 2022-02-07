f381fa76c608a59b2dd75b05c6fee70f5f50ff4e
Rick Huijzer <ikbenrickhuijzer@gmail.com>
2022-02-07 21:45:30 +0100

    Next version; see dwm repo commit log

--- brandhout/packages/brandhout-dwm.scm
+++ brandhout/packages/brandhout-dwm.scm
@@ -12,7 +12,7 @@
         (method git-fetch)
         (uri (git-reference
           (url "https://github.com/brandhout/poor_dwm_fork.git")
-          (commit "8a2ae19945a78cb0302f825ae26eacd1ed9667b0")))
+          (commit "a697f583f5aa69e74027accd3b5b6a84a1e1b6ee")))
         (sha256
-          (base32 "1vy9np307c5hpwgap2ivn5qcvdjpjl4y24mia5i39k1f3admjmgf"))))))
+          (base32 "1w5f29jiinngdrbxblckb0xx79zgvdgzfd5vcg7r9fn5az813c63"))))))
 
