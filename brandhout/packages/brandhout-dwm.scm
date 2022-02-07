2abd0938c05d999ef81a00f99349d5416aade74a
Rick Huijzer <ikbenrickhuijzer@gmail.com>
2022-02-07 19:01:00 +0100

    FIX; wrong commit

--- brandhout/packages/brandhout-dwm.scm
+++ brandhout/packages/brandhout-dwm.scm
@@ -12,7 +12,7 @@
         (method git-fetch)
         (uri (git-reference
           (url "https://github.com/brandhout/poor_dwm_fork.git")
-          (commit "427984fbf22ca8655f2b262ac0474176bb86af6")))
+          (commit "8a2ae19945a78cb0302f825ae26eacd1ed9667b0")))
         (sha256
           (base32 "1vy9np307c5hpwgap2ivn5qcvdjpjl4y24mia5i39k1f3admjmgf"))))))
 
