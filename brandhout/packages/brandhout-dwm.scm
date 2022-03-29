62b4ebefc8d30a8d1db154bb47ff5bf3a3d4c5af
Rick Huijzer <ikbenrickhuijzer@gmail.com>
2022-03-29 15:34:13 +0200

    Up to date with repository

--- brandhout/packages/brandhout-dwm.scm
+++ brandhout/packages/brandhout-dwm.scm
@@ -14,5 +14,5 @@
           (url "https://github.com/brandhout/poor_dwm_fork.git")
           (commit "a697f583f5aa69e74027accd3b5b6a84a1e1b6ee")))
         (sha256
-          (base32 "1w5f29jiinngdrbxblckb0xx79zgvdgzfd5vcg7r9fn5az813c63"))))))
+          (base32 "0mid4ckp9pc18c1yksw4g8k1qama5w866dqnfzm6bnn98aw130vq"))))))
 
